8757ea6ca615399f6add7734d1a48b606cbd8890
AliFareed0009 <alifareed0009@gmail.com>
2024-12-05 19:17:06 +0500

    Adding READme file

---
+++ Projects/Java_Sample_App/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, From Docker Container! This is Java Application showing Current date: " + currentDate);
+    }
+}
